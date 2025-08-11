7436962be07012c0ccd02b8a802f1064dd25c1e8
Ndg19 <158159787+Ndg19@users.noreply.github.com>
2025-08-11 21:49:16 +0300

    modular css implimented

--- src/components/AllFoodItems/AllFooditems.jsx
+++ src/components/AllFoodItems/AllFooditems.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import "../FoodItem/FoodItem.css";
+import "../AllFoodItems/AllFoodItems.css";
 // 1. Define props type
 // interface AllFoodItemsProps {
 //   img: string;
